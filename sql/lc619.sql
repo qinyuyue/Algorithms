ac6c5f3f39f01709254eab5d76fc69b8b082c9aa
qinyuyue <iamyqy@hotmail.com>
2019-05-07 23:20:28 -0700

    Biggest unique number, 66%

---
+++ sql/lc619.sql
@@ -0,0 +1,30 @@
+/*
+Biggest unique number 
+Table my_numbers contains many numbers in column num including duplicated ones.
+Can you write a SQL query to find the biggest number, which only appears once.
+
++---+
+|num|
++---+
+| 8 |
+| 8 |
+| 3 |
+| 3 |
+| 1 |
+| 4 |
+| 5 |
+| 6 |
+For the sample data above, your query should return the following result:
++---+
+|num|
++---+
+| 6 |
+*/
+-- 66%
+# Write your MySQL query statement below
+SElECT MAX(num) AS num
+FROM
+(SELECT num
+FROM my_numbers
+GROUP BY num
+HAVING count(num) = 1) AS t
