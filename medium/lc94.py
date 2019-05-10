f6c117465c95b242458ff7c2e58a6cd78533605e
qinyuyue <iamyqy@hotmail.com>
2019-05-10 00:54:18 -0700

    Binary Tree Inorder Traversal, 84%

---
+++ medium/lc94.py
@@ -0,0 +1,52 @@
+## Binary Tree inorder Traversal 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        """
+        Algorithm:
+        1. initialize a stack, visited set.
+        2. while len of stack > 0,
+           for each node
+            if node.left exit and was not explored, node = node.left
+            elif node.right exit and was not explored, node = node.right
+            else, break
+        3. add the current node to stack
+        """
+        output = []
+        if root:
+            stack = [root]
+
+            visited = set()
+            node = root
+            while stack:
+                # print([i.val for i in stack])
+                # print('output', output)
+                # print('visited', [i.val for i in visited])
+                node = stack[-1]
+
+                while node:
+                    # print(node.val)
+                    # print([i.val for i in stack])
+                    if node.left and node.left not in visited:
+                        stack.append(node.left)
+                        node = node.left
+                    elif node.right and node.right not in visited:
+                        x = stack.pop(-1)
+                        output.append(x.val)
+                        visited.add(x)
+                        stack.append(node.right)
+                        node = node.right
+                    else:
+                        visited.add(node)
+                        break
+                node = stack.pop(-1)
+                output.append(node.val)
+
+        return output
