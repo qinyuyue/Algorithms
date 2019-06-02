5d7d176caa8fed192500d399a92351f6293be5e4
qinyuyue <iamyqy@hotmail.com>
2019-06-02 16:24:06 -0700

    Array Partition, O(n)

--- easy/lc561.py
+++ easy/lc561.py
@@ -2,6 +2,9 @@
 
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
+        """
+        Sorted I
+        """
         # if len(nums) == 0:
         #     return 0
         # sort_nums = sorted(nums)
@@ -14,22 +17,38 @@ class Solution:
         #         # print(sums)
         # return sums
 
-        sums = 0
-        if nums:
-            nums = sorted(nums)
-            odd = False
-            i = 0
-            j = len(nums) - 2
-            n = len(nums) // 2
-            if n % 2 == 1:
-                odd = True
-            while i < j:
-                sums += nums[i]
-                sums += nums[j]
-                i += 2
-                j -= 2
-            if odd == True:
-                sums += nums[i]
-        return sums
+        """
+        Sorted II
+        """
+        # sums = 0
+        # if nums:
+        #     nums = sorted(nums)
+        #     odd = False
+        #     i = 0
+        #     j = len(nums) - 2
+        #     n = len(nums) // 2
+        #     if n % 2 == 1:
+        #         odd = True
+        #     while i < j:
+        #         sums += nums[i]
+        #         sums += nums[j]
+        #         i += 2
+        #         j -= 2
+        #     if odd == True:
+        #         sums += nums[i]
+        # return sums
 
-                
+        """
+        O(n)
+        a array to store the frequency of -10000 ~ 10000
+        """
+        arr = [0] * 20001
+        lmt = 10000
+        sums = 0
+        d = 0
+        for n in nums:
+            arr[n+lmt] += 1
+        for i in range(-10000, 10000):
+            sums += (arr[i+lmt] + 1 - d % 2) * i
+            d = (2 + arr[i+lmt] - d) % 2
+        return sums 
