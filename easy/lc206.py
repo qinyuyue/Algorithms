64c18aa9971cde637a0da6a520ee6f1402ad1e4d
qinyuyue <iamyqy@hotmail.com>
2019-06-04 17:59:46 -0700

    Reverse Linked List, O(n)

--- easy/lc206.py
+++ easy/lc206.py
@@ -1,85 +1,32 @@
-## Designed linked list 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.next = None
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
 
-class MyLinkedList:
+        """
+        Algorithm - Iterative
+        1. prev, curr, head
+        2. curr = head, curr.next = prev, prev = curr
+        """
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
 
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.head = None
-        self.size = 0
+        return prev
 
-    def get(self, index: int) -> int:
         """
-        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        Algorithm - Recursive
         """
-        if index < 0 or index >= self.size:
-            return -1
-        if self.head == None:
-            return -1
-        else:
-            curr = self.head
-            for i in range(index):
-                curr = curr.next
-            return curr.val
-
-    def addAtHead(self, val: int) -> None:
-        """
-        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
-        """
-        self.addAtIndex(0, val)
-
-    def addAtTail(self, val: int) -> None:
-        """
-        Append a node of value val to the last element of the linked list.
-        """
-        self.addAtIndex(self.size, val)
-
-    def addAtIndex(self, index: int, val: int) -> None:
-        """
-        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
-        """
-        if index <= self.size:
-            node = Node(val)
-            if index <= 0:
-                node.next = self.head
-                self.head = node
-            else:
-                curr = self.head
-                if curr == None:
-                    self.head = node
-                else:
-                    for i in range(index - 1):
-                        curr = curr.next
-                    node.next = curr.next
-                    curr.next = node
-            self.size += 1
-
-
-    def deleteAtIndex(self, index: int) -> None:
-        """
-        Delete the index-th node in the linked list, if the index is valid.
-        """
-        if 0 <= index < self.size:
-            curr = self.head
-            if index == 0:
-                self.head = curr.next
-            else:
-                for i in range(index - 1):
-                    curr = curr.next
-                curr.next = curr.next.next
-            self.size -= 1
-
-
-# Your MyLinkedList object will be instantiated and called as such:
-# obj = MyLinkedList()
-# param_1 = obj.get(index)
-# obj.addAtHead(val)
-# obj.addAtTail(val)
-# obj.addAtIndex(index,val)
-# obj.deleteAtIndex(index)
+        if head == None or head.next == None:
+            return head
+        curr = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return curr
